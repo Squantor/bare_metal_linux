88d0750f8a3371848ce2b5501b903f17d8fed5a3
Bart Bilos <anton.bilos@gmail.com>
2016-11-29 23:16:58 +0100

    * add some additional code

--- bare_metal_linux_C/main.c
+++ bare_metal_linux_C/main.c
@@ -1,10 +1,8 @@
-#include <stdint.h>
-
 static inline long syscall1(long syscall, long arg1)
 {
-long ret;
-asm volatile ("int $0x80":"=a"(ret):"a"(syscall),"b"(arg1):"memory");
-return ret;
+    long ret;
+    asm volatile ("int $0x80":"=a"(ret):"a"(syscall),"b"(arg1):"memory");
+    return ret;
 }
 
 static inline long syscall3(long syscall, long arg1, long arg2, long arg3)
