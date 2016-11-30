43dc32eee293fc7b0b98ab0a67dd4c8d06a6c018
Bart Bilos <anton.bilos@gmail.com>
2016-11-30 22:45:58 +0100

    * some more dir movements, Makefile settings

--- bare_metal_x86_linux_C/src/main.c
+++ bare_metal_x86_linux_C/src/main.c
@@ -1,16 +1,4 @@
-static inline long syscall1(long syscall, long arg1)
-{
-    long ret;
-    asm volatile ("int $0x80":"=a"(ret):"a"(syscall),"b"(arg1):"memory");
-    return ret;
-}
-
-static inline long syscall3(long syscall, long arg1, long arg2, long arg3)
-{
-    long ret;
-    asm volatile ("int $0x80" : "=a" (ret) : "a" (syscall), "b" (arg1), "c" (arg2),"d" (arg3) : "memory");
-    return ret;
-}
+#include <syscalls.h>
 
 int write(int fd, const void *buf, int count)
 {
