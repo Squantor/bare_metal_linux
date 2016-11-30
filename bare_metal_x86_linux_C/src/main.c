5942d619467e7d28cd72fef330b3a69082da266c
Bart Bilos <anton.bilos@gmail.com>
2016-11-30 23:01:55 +0100

    * some mods

--- bare_metal_x86_linux_C/src/main.c
+++ bare_metal_x86_linux_C/src/main.c
@@ -5,14 +5,9 @@ int write(int fd, const void *buf, int count)
     return syscall3(4, fd, (long)buf, count);
 }
 
-void exit(int status)
-{
-    syscall1(1, status);
-}
-
-void _start()
+int main(int argc, char const* argv[])
 {
     int retval;
     retval = write(1,"Hello world\n",12);
-    exit(0);
+    return retval;
 }
